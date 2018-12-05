2d6a7d31478a551843f7e5ce07181673ead8aef1
siddharth178 <siddharth.karandikar@gmail.com>
2018-12-05 17:43:44 +0530

    allow white-list of paths to be excluded from redirection
    sometimes we would want certain paths to go through over plain HTTP.
    allow those paths by configuring in the middleware at the time of
    creating the middleware.
    
    white-listed paths are case-sensitive.

--- redirect/redirect.go
+++ redirect/redirect.go
@@ -8,11 +8,24 @@ import (
 )
 
 // SecureRedirectMiddleware redirects the client to the identical URL served via HTTPS
-type SecureRedirectMiddleware struct{}
+type SecureRedirectMiddleware struct {
+	WhiteListedPaths map[string]struct{}
+}
 
-func (SecureRedirectMiddleware) MiddlewareFunc(h rest.HandlerFunc) rest.HandlerFunc {
+func NewSecureRedirectMiddleware(paths ...string) SecureRedirectMiddleware {
+	uniquePaths := make(map[string]struct{})
+	for _, p := range paths {
+		uniquePaths[p] = struct{}{}
+	}
+	return SecureRedirectMiddleware{
+		WhiteListedPaths: uniquePaths,
+	}
+}
+
+func (srm SecureRedirectMiddleware) MiddlewareFunc(h rest.HandlerFunc) rest.HandlerFunc {
 	return func(w rest.ResponseWriter, r *rest.Request) {
-		if strings.ToLower(r.Header.Get("X-Forwarded-Proto")) == "http" {
+		_, whiteListed := srm.WhiteListedPaths[r.URL.Path]
+		if strings.ToLower(r.Header.Get("X-Forwarded-Proto")) == "http" && !whiteListed {
 			redirectURL := r.URL
 			redirectURL.Host = r.Host
 			redirectURL.Scheme = "https"
