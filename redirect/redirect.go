0211c8f7e16298221faee7483f3721b9b6900750
siddharth178 <siddharth.karandikar@gmail.com>
2018-12-06 18:54:24 +0530

    better names to indicate use of these paths

--- redirect/redirect.go
+++ redirect/redirect.go
@@ -9,22 +9,22 @@ import (
 
 // SecureRedirectMiddleware redirects the client to the identical URL served via HTTPS
 type SecureRedirectMiddleware struct {
-	WhiteListedPaths map[string]struct{}
+	AllowedInsecurePaths map[string]struct{}
 }
 
-func NewSecureRedirectMiddleware(paths ...string) SecureRedirectMiddleware {
+func NewSecureRedirectMiddleware(allowedInsecurepaths ...string) SecureRedirectMiddleware {
 	uniquePaths := make(map[string]struct{})
-	for _, p := range paths {
+	for _, p := range allowedInsecurepaths {
 		uniquePaths[p] = struct{}{}
 	}
 	return SecureRedirectMiddleware{
-		WhiteListedPaths: uniquePaths,
+		AllowedInsecurePaths: uniquePaths,
 	}
 }
 
 func (srm SecureRedirectMiddleware) MiddlewareFunc(h rest.HandlerFunc) rest.HandlerFunc {
 	return func(w rest.ResponseWriter, r *rest.Request) {
-		_, whiteListed := srm.WhiteListedPaths[r.URL.Path]
+		_, whiteListed := srm.AllowedInsecurePaths[r.URL.Path]
 		if strings.ToLower(r.Header.Get("X-Forwarded-Proto")) == "http" && !whiteListed {
 			redirectURL := r.URL
 			redirectURL.Host = r.Host
